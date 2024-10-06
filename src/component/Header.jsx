2a50fd524f3d448d1d1158420d3a23205663b191
Abdulrahman <remaxi135@gmail.com>
2024-10-06 08:02:08 +0100

    Update Header.jsx

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -10,9 +10,9 @@ export const Header = ({background}) => {
     <div style={{ backgroundImage: `url(${background})` }} className='max-h-screen max-w-full flex items-center mx-auto w-screen h-screen md:h-full md:py-40 lg:py-0 lg:pb-0 lg:h-screen bg-cover bg-center text-lg font-light  ' >
         <div className=' w-full max-w-md md:max-w-4xl lg:max-w-7xl mx-auto space-y-20 '>
             <div className='space-y-5 px-4 xl:px-0'>
-            <p>We're here to help you</p>
+            <p className='text-[#12305B]'>We're here to help you</p>
             <h1 className='text-7xl text-[#12305B] font-bold font-poppins'>Relax & Rest</h1>
-            <p>With the aid of our Melatonin Sleepstiq, we can assure you can get quality sleep</p>
+            <p className='text-[#12305B]'>With the aid of our Melatonin Sleepstiq, we can assure you can get quality sleep</p>
 
             <Button text={"Visit Shop"} />
             </div>
